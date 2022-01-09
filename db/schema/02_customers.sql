7f4369ad2603f798c5542f53cfecf25462c5143d
Daniel Lu <daniel_lu@hotmail.com>
2022-01-09 16:28:56 -0500

    edit schema folder to include 5 tables

--- db/schema/02_customers.sql
+++ db/schema/02_customers.sql
@@ -1,5 +1,9 @@
+-- Drop and recreate customers table
+
 DROP TABLE IF EXISTS customers CASCADE;
+
 CREATE TABLE customers (
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   phone_number VARCHAR(32) NOT NULL
 );
---
+++ db/schema/05_order_food_items.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate order_food_items table
+
+DROP TABLE IF EXISTS order_food_items CASCADE;
+
+CREATE TABLE order_food_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  food_item_id INTEGER REFERENCES food_items(id) ON DELETE CASCADE,
+  order_id INTERGER REFERENCES orders(id) ON DELETE CASCADE,
+  quantity SMALLINT
+);
