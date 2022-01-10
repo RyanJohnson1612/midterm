06f8fec0481008a0d72c378357fa914900eb72e4
Daniel Lu <daniel_lu@hotmail.com>
2022-01-10 17:26:42 -0500

    edit error in cusomters seeds

--- db/seeds/02_customers.sql
+++ db/seeds/02_customers.sql
@@ -1,6 +1,6 @@
 -- customers table seeds here
 
-INSERT INTO customers (name, phone)
+INSERT INTO customers (name, phone_number)
 VALUES ('Pascal', '6471111111'),
 ('Nurse', '6472222222'),
 ('VanVleet', '6473333333'),
