f9ab71b28892c4fa65aa3d611ff308b839662f68
Najib <muhammed.najib.farah@gmail.com>
2022-01-11 23:00:31 +0000

    backend stuff

--- db/schema/07_food_iteams.sql
+++ db/schema/07_food_iteams.sql
@@ -5,6 +5,6 @@ CREATE TABLE food_items (
   price INTEGER NOT NULL,
   picture_url VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  spice_level VARCHAR(32) DEFAULT NONE,
+  spice_level VARCHAR(32) DEFAULT NULL,
   size VARCHAR(32) DEFAULT M
 );
