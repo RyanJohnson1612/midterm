4911c190c268b631e8ad72516e08725420b336af
Daniel Lu <daniel_lu@hotmail.com>
2022-01-10 20:03:53 -0500

    edit restaurants table to have column phone_number and edit restaurants seeds to Burger&Friends restaurant having phone_number, 2262239961

--- db/schema/01_restaurants.sql
+++ db/schema/01_restaurants.sql
@@ -6,5 +6,6 @@ CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   email VARCHAR (255) NOT NULL,
   password VARCHAR (255) NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(32) NOT NULL
 );
