6f9ef1a1e9e0e85d08aeeb801b7ce60a9b8cd347
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-01-11 05:50:04 +0000

    Change scss files to partial that compile to a single css file, added head partial, and remove widget routes

--- server.js
+++ server.js
@@ -36,12 +36,11 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const restaurantsRoutes = require("./routes/restaurants");
-const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/restaurants", restaurantsRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
